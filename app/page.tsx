915fa3e5f444648a974771d4f2fd40722dbc7754
Vignesh <joshivignesh@users.noreply.github.com>
2023-10-10 20:14:59 +0530

    Changed layout

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ Space Jelly Shop            <span className="inline-block transition-transform g
               -&gt;
             </span>
           </h2>
-      {/* <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      { <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -29,7 +29,7 @@ Space Jelly Shop            <span className="inline-block transition-transform g
           </p>
         </a>
 
-      </div> */}
+      </div> }
     </main>
   )
 }
